4be9f92ef0746b454b0b05691e89f4a5d404782f
app_3872b97a <app_3872b97a@ip-10-235-45-61.ec2.internal>
2014-07-11 02:50:47 +0000

    AndroidKickstartR generation

---
+++ MyApplication/src/com/androidkickstarter/app/MainActivity.java
@@ -0,0 +1,25 @@
+
+package com.androidkickstarter.app;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity
+    extends SherlockActivity
+{
+
+
+    @AfterViews
+    void afterViews() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater();
+        return true;
+    }
+
+}
